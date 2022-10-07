d4fd3ee6d54f38731bf17dc2a136d7aec5ba4b32
Moina Vernon <mvernon@cpp.edu>
2022-10-07 14:27:00 -0700

    created endpoints

--- src/main/python/app.py
+++ src/main/python/app.py
@@ -1,6 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify, redirect, url_for
 
 app = Flask(__name__)
+
+@app.route("/")
+def d1():
+	return "Desktop - 1"
+
+@app.route("/2")
+def d2():
+	return "Desktop - 2"
+
+@app.route("/3")
+def d3():
+	return "Desktop - 3"
+
+@app.route("/1")
+def go_to_one():
+	return redirect(url_for("d1"))
+
 
 if __name__=='__main__':
 	app.run(debug=True, port=8000)
