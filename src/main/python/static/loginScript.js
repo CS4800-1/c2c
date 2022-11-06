0f4733855cdad1d3eded31dccf277f9ee8cc03f3
Moina Vernon <mvernon@cpp.edu>
2022-11-05 22:48:35 -0700

    login with google auth

---
+++ src/main/python/static/loginScript.js
@@ -0,0 +1,47 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithRedirect, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyAaxnJ-eYsIC9A1g_6wAIWCPeRElEVBG3g",
+	authDomain: "coffee2conference.firebaseapp.com",
+	databaseURL: "https://coffee2conference-default-rtdb.firebaseio.com",
+	projectId: "coffee2conference",
+	storageBucket: "coffee2conference.appspot.com",
+	messagingSenderId: "718465301534",
+	appId: "1:718465301534:web:17e424f8ec38caea5a020f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider(app);
+const auth = getAuth(app);
+
+googleLogin.addEventListener('click', (e) => {
+	// signInWithRedirect(auth, provider);
+	signInWithPopup(auth, provider)
+		.then((result) => {
+			// This gives you a Google Access Token. You can use it to access the Google API.
+			const credential = GoogleAuthProvider.credentialFromResult(result);
+			const token = credential.accessToken;
+			// The signed-in user info.
+			const user = result.user;
+			alert(user.displayName);
+			// ...
+		}).catch((error) => {
+			// Handle Errors here.
+			const errorCode = error.code;
+			const errorMessage = error.message;
+			// The email of the user's account used.
+			const email = error.customData.email;
+			// The AuthCredential type that was used.
+			const credential = GoogleAuthProvider.credentialFromError(error);
+			// ...
+			alert(errorMessage);
+		});
+
+});
+
