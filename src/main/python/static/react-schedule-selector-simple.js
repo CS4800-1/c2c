7df942f12e179e8031855b85234f196acc4166aa
Moina Vernon <mvernon@cpp.edu>
2022-11-21 21:22:23 -0800

    schedule selector

---
+++ src/main/python/static/react-schedule-selector-simple.js
@@ -0,0 +1,18 @@
+import ScheduleSelector from 'react-schedule-selector'
+
+class App extends React.Component {
+	state = { schedule : [] }
+
+	handleChange = newSchedule => {
+		this.setState({ schedule: newSchedule })
+	}
+
+	render() {
+		return (
+			<ScheduleSelector
+				selection={this.state.schedule}
+				onChange={this.handleChange}
+			/>
+		)
+	}
+}
