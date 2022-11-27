3ca3b02bc0e71366ca62cbf34d672c84b2d74867
palakmalik5 <palakmalik5pm@gmail.com>
2022-11-27 15:29:42 -0800

    Update react-schedule-selector-simple.js

--- src/main/python/static/react-schedule-selector-simple.js
+++ src/main/python/static/react-schedule-selector-simple.js
@@ -1,18 +1,18 @@
-import ScheduleSelector from 'react-schedule-selector'
+import ScheduleSelector from "react-schedule-selector";
 
 class App extends React.Component {
-	state = { schedule = [] }
+  state = { schedule: [] };
 
-	handleChange = newSchedule => {
-		this.setState({ schedule: newSchedule })
-	}
+  handleChange = (newSchedule) => {
+    this.setState({ schedule: newSchedule });
+  };
 
-	render() {
-		return (
-			<ScheduleSelector
-				selection={this.state.schedule}
-				onChange={this.handleChange}
-			/>
-		)
-	}
+  render() {
+    return (
+      <ScheduleSelector
+        selection={this.state.schedule}
+        onChange={this.handleChange}
+      />
+    );
+  }
 }
