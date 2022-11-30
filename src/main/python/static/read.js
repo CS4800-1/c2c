febfe6cdedf8d121a8a26df027c2c118b19a6343
Moina Vernon <mvernon@cpp.edu>
2022-11-30 10:26:10 -0800

    finished read.js

--- src/main/python/static/read.js
+++ src/main/python/static/read.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
-import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-database.js";
+import { getDatabase, ref, get, child } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-database.js";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyAaxnJ-eYsIC9A1g_6wAIWCPeRElEVBG3g",
@@ -17,26 +17,62 @@ const uniqueId = document.getElementById('unique_id');
 
 //use to show group availibilty calendar
 //read database Events/.../.../availableTimes - initially empty
+
 function readData() {
 	const db = getDatabase();
-	console.log(uniqueId.value);
-	const firebaseRef = ref(db, `/${uniqueId.value}/userTimes/`); //-NHNQvVWLDx7e-lbpVHf');
-	// onValue(firebaseRef, (snapshot) => {
-	// 	console.log(snapshot.val())
-	// 	object.innerHTML = `<div>${snapshot.val()}</div>`
-	// });
-	object.innerHTML = `<div>`
-	onValue(firebaseRef, (snapshot) => {
-		snapshot.forEach(element => {
-			console.log(element.val());
+	// console.log(uniqueId.value);
 
-			object.innerHTML += `${element.val()}\n`
-		});
-		object.innerHTML += `</div>`
-		// const data = snapshot.val();
-	}
+	object.innerHTML = ``;
+	//display event name
+	const dbRef = ref(db);
+	get(child(dbRef, `/${uniqueId.value}/eventName`)).then((snapshot) => {
+		if (snapshot.exists()) {
+			console.log(snapshot.val());
+			object.innerHTML = `<h2> Showing Group Availability for ${snapshot.val()}\n </h2>`;
 
-	);
+		}
+		else {
+			object.innerHTML = `<h2> Unique ID "${uniqueId.value}" not found\n </h2>`;
+	
+		}
+	}).catch((error) => {
+		console.error(error);
+	});
+
+
+	//display person with their choosen date(s) with times
+	const firebaseRef = ref(db, `/${uniqueId.value}/userTimes`); //-NHNQvVWLDx7e-lbpVHf');
+
+
+	// get(firebaseRef, (snapshot) => {
+	get(firebaseRef).then((snapshot) => {
+		if (snapshot.exists()) {
+			snapshot.forEach(element => {
+				const entries = Object.entries(element.val());
+
+				console.log(element.key);
+				console.log(entries + "\n----------")
+
+				object.innerHTML += `<h3>${element.key} : \n</h3>`
+
+				entries.forEach(item => {
+					var vals = Object.values(item)
+					var times = Object.values(vals[1])
+					// console.log(vals[0]) //log choosen date, always first key
+					// console.log(times)	//log chossen times (array)
+
+					object.innerHTML += `<b>\t${vals[0]}\n\t\t[${times}]\n </b>`
+
+					// console.log("**********")
+
+				});
+			});
+		}
+		else {
+			object.innerHTML += `<b> No times </b>`;
+
+		}
+	});
 }
 
 
