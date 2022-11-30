1a037eb723dc68d160b3afe5005e1c2494f720dd
Jasmine Weddle <67066904+coffeenated@users.noreply.github.com>
2022-11-29 22:29:12 -0800

    add time update functionality

--- src/main/python/static/update.js
+++ src/main/python/static/update.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
 import { getDatabase, ref, set, push } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-database.js";
 
 const firebaseConfig = {
@@ -15,3 +15,51 @@ const app = initializeApp(firebaseConfig);
 
 //use when user is auth, enters uniqueId, and selects their available times
 //read database Events/eventID/userTimes/person/<array> - initially doesn't exist
+
+
+
+
+var start_time = document.querySelector("#start-time");
+var end_time = document.querySelector("#end-time");
+var event_id = document.querySelector("#unique-id");
+var user_name = document.querySelector("#person-name");
+var date = document.querySelector("#date");
+
+
+function setTimes(event) { //name, sTime, eTime) {
+	event.preventDefault();
+
+	const db = getDatabase();
+	var reference = ref(db, event_id).child("userTimes").child(user_name).child(date);
+	const time = [start_time,end_time]
+
+	if (reference != null)
+	{
+		reference.push(time);
+	}
+	else
+	{
+		refrence = time;
+	}
+
+	set(ref(db,event_id).child("userTimes").child(user_name).child(date),{
+		times: time;
+
+		}).then(() => {
+		alert("Times added: " + time)
+	})
+	.catch((error) => {
+		alert(error)
+	});
+	
+
+
+
+
+
+}
+
+
+var button_submit = document.querySelector("#submit-avail")
+button_submit.addEventListener('click', setTimes)
+
